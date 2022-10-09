83c70328857b060daf8962afe3fad4ecf2bff249
NoahTheCorgi <91349811+NoahTheCorgi@users.noreply.github.com>
2022-10-09 18:13:10 -0400

    Create containerMostWater.py

---
+++ pointers/containerMostWater.py
@@ -0,0 +1,14 @@
+# container with most water problem solution version 5
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        length = len(height)
+        left, right = 0, length-1
+        maxamount = min(height[left], height[right]) * (right - left)
+        while (left < right):
+            if (height[left] > height[right]): # move the right inward
+                right -= 1
+            else:
+                left += 1
+            maxamount = max(maxamount, min(height[left], height[right]) * (right - left))
+        return maxamount
