993122f6bf633885dfe8fb96a5526aa330f67eb0
NoahTheCorgi <91349811+NoahTheCorgi@users.noreply.github.com>
2022-11-08 16:06:38 -0500

    initiated linked list algorithms

---
+++ pointers/linkedlists/linkedlist_has_cycle.py
@@ -0,0 +1,31 @@
+# LinkedList has cycle?
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # if there is a cycle will find since gcd(1,2) = 1
+        # if there is no cycle, guaranteed to terminate 
+        slow, fast = head, head
+        while (fast != None and fast.next !=None):
+            slow = slow.next
+            fast = fast.next.next
+            if (slow == fast):
+                return True
+        return False
+        
+        # Below is an implementation that is slower
+        # helper = set()
+        # pointer = head
+        # while (pointer!=None and pointer.next != None):
+        #     if (pointer in helper):
+        #         return True
+        #     pointercopy = pointer
+        #     helper.add(pointercopy)
+        #     pointer = pointer.next
+        # return False
+    
